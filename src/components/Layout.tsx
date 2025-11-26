16d636dc9e9db1c4e6a3087514a0747b5b47d163
ryna6 <ryanjw629.h@gmail.com>
2025-11-26 04:29:43 -0500

    Update Layout.tsx

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,1 +1,16 @@
+import React from "react";
+
+interface LayoutProps {
+  children: React.ReactNode;
+}
+
+export const Layout: React.FC<LayoutProps> = ({ children }) => {
+  return (
+    <div className="app">
+      <main className="app__content">{children}</main>
+    </div>
+  );
+};
+
+export default Layout;
 
