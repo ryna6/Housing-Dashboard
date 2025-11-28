5d78de89dcdcdf30e46dc6ef0c14f6f48dc76b4c
ryna6 <ryanjw629.h@gmail.com>
2025-11-28 05:13:09 -0500

    Update regions.ts

--- src/data/regions.ts
+++ src/data/regions.ts
@@ -1,38 +1,8 @@
-import type { MarketCode, RegionCode } from "./types";
-
-// Regions grouped by market for the Rentals / Sales tabs
-export const REGIONS_BY_MARKET: Record<MarketCode, RegionCode[]> = {
-  canada: [],
-  on: ["gta", "hamilton", "halton", "niagara", "greater_toronto"],
-  bc: [
-    "vancouver",
-    "burnaby",
-    "surrey",
-    "richmond",
-    "victoria",
-    "greater_vancouver",
-    "lower_mainland",
-  ],
-};
+import type { RegionCode } from "./types";
 
 export const REGION_LABELS: Record<RegionCode, string> = {
   // Markets
   canada: "Canada",
-  on: "Ontario",
-  bc: "British Columbia",
-
-  // Original city / region codes
-  gta: "GTA",
-  hamilton: "Hamilton",
-  halton: "Halton",
-  niagara: "Niagara",
-  vancouver: "Vancouver",
-  burnaby: "Burnaby",
-  surrey: "Surrey",
-  richmond: "Richmond",
-  victoria: "Victoria",
-
-  // New aggregate regions for the Prices tab
   greater_vancouver: "Greater Vancouver",
   lower_mainland: "Lower Mainland (Burnaby, Surrey, New West, Coquitlam)",
   calgary: "Calgary",
