32258158b5ce93be23ffa14786543b655d4411b7
ryna6 <ryanjw629.h@gmail.com>
2025-11-28 03:32:48 -0500

    Update regions.ts

--- src/data/regions.ts
+++ src/data/regions.ts
@@ -1,26 +1,6 @@
-import type { MarketCode, RegionCode } from "./types";
-
-export const REGIONS_BY_MARKET: Record<MarketCode, RegionCode[]> = {
-  canada: [],
-  on: ["gta", "hamilton", "halton", "niagara"],
-  bc: ["vancouver", "burnaby", "surrey", "richmond", "victoria"],
-};
-
 export const REGION_LABELS: Record<RegionCode, string> = {
   canada: "Canada",
-  on: "Ontario",
-  bc: "British Columbia",
-  gta: "GTA",
-  hamilton: "Hamilton",
-  halton: "Halton",
-  niagara: "Niagara",
-  vancouver: "Vancouver",
-  burnaby: "Burnaby",
-  surrey: "Surrey",
-  richmond: "Richmond",
-  victoria: "Victoria",
-
-  // Additional regions available on Prices tab
+  greater_vancouver: "Vancouver",
   lower_mainland: "Lower Mainland",
   calgary: "Calgary",
   greater_toronto: "Greater Toronto Area",
