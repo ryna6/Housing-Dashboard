e6c06bf55eddc92c92ecfe5c59d045aadca34048
ryna6 <ryanjw629.h@gmail.com>
2025-11-28 04:02:36 -0500

    Update types.ts

--- src/data/types.ts
+++ src/data/types.ts
@@ -1,13 +1,34 @@
+// Top-level market buckets used by MarketSelector on other tabs
+export type MarketCode = "canada" | "on" | "bc";
+
+// All region codes used anywhere in the app
+// - Original city-level regions (GTA, Hamilton, etc.) for Rentals/Sales
+// - New aggregate regions for the Prices tab (greater_vancouver, lower_mainland, etc.)
 export type RegionCode =
   | MarketCode
-  | "canada"
+  | "gta"
+  | "hamilton"
+  | "halton"
+  | "niagara"
+  | "vancouver"
+  | "burnaby"
+  | "surrey"
+  | "richmond"
+  | "victoria"
+  // Aggregate / new regions for the Prices tab
   | "greater_vancouver"
   | "lower_mainland"
   | "calgary"
   | "greater_toronto"
   | "montreal";
 
+// Segment values:
+// - "all" / "condo" / "freehold" are used by the SalesListings tab
+// - The housing-type segments are used by the new Prices tab
 export type Segment =
+  | "all"
+  | "condo"
+  | "freehold"
   | "composite"
   | "one_storey"
   | "two_storey"
