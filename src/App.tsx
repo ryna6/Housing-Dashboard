b489469db1c382530cff62842dc8aa8217cf68b6
ryna6 <ryanjw629.h@gmail.com>
2025-12-02 22:55:13 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import React, { useState } from "react";
+import type { TabKey } from "./tabs/tabConfig";
+
 import { OverviewTab } from "./tabs/OverviewTab";
 import { PricesTab } from "./tabs/PricesTab";
 import { SalesListingsTab } from "./tabs/SalesListingsTab";
@@ -9,6 +11,47 @@ import { CreditTab } from "./tabs/CreditTab";
 import { MarketTab } from "./tabs/MarketTab";
 import { RentalsTab } from "./tabs/RentalsTab";
 import { BottomTabNav } from "./components/BottomTabNav";
+
+// Allows overview tab to change tabs when clicking a card
+const App: React.FC = () => {
+  const [activeTab, setActiveTab] = useState<TabKey>("overview");
+
+  const renderTab = () => {
+    switch (activeTab) {
+      case "overview":
+        return <OverviewTab onNavigateTab={setActiveTab} />;
+      case "prices":
+        return <PricesTab />;
+      case "sales":
+        return <SalesListingsTab />;
+      case "supply":
+        return <SupplyTab />;
+      case "rates":
+        return <RatesBondsTab />;
+      case "inflation":
+        return <InflationLabourTab />;
+      case "credit":
+        return <CreditTab />;
+      case "market":
+        return <MarketTab />;
+      case "rentals":
+        return <RentalsTab />;
+      default:
+        return <OverviewTab onNavigateTab={setActiveTab} />;
+    }
+  };
+
+  return (
+    <div className="app">
+      <main className="app__content">
+        <div className="app__inner">{renderTab()}</div>
+      </main>
+      <BottomTabNav active={activeTab} onChange={setActiveTab} />
+    </div>
+  );
+};
+
+export default App;
 
 /**
  * Tab IDs must match what BottomTabNav uses internally.
